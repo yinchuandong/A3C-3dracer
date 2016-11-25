1d31d71d6ec6ee4e1e02bf7bc05c3f4c5fc3adfb
yinchuandong <johncdyin@gmail.com>
2016-11-25 14:23:20 +1100

    new: init lib

---
+++ test_client.py
@@ -0,0 +1,68 @@
+import socket
+import sys
+import numpy as np
+import cPickle
+import time
+import base64
+import json
+
+
+HOST, PORT = "localhost", 9999
+# HOST, PORT = "10.12.185.71", 9999
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def encode_img(filename):
+    b64 = base64.encodestring(open(filename, "rb").read())
+    return b64
+
+
+def main():
+    img = encode_img('test.png')
+    data = {
+        'img': img,
+        'reward': 0.1
+    }
+    data = json.dumps(data)
+
+    sock.connect((HOST, PORT))
+    send_msg(sock, data)
+    received = sock.recv(100)
+    sock.close()
+    print("Received: {}".format(received))
+
+    return
+
+
+def send_msg(sock, msg):
+    # Prefix each message with a 4-byte length (network byte order)
+    msg = str(len(msg)).zfill(20) + msg
+    sended_len = 0
+    while sended_len < len(msg):
+        left = len(msg) - sended_len
+        offset = 8000 if left > 8000 else left
+        sock.sendall(msg[sended_len: sended_len + offset])
+        sended_len += offset
+    return
+
+
+
+
+if __name__ == '__main__':
+    # size = len(encode_img('test.png'))
+    # print size
+    # print str(123456).zfill(10)
+    main()
+    # for i in range(1000):
+    #     main()
+    #     
+        
+    # with open('test_0.txt', 'w') as f:
+    #     img = encode_img('test.png')
+    #     data = {
+    #         'img': img,
+    #         'reward': 0.1
+    #     }
+    #     data = json.dumps(data)
+    #     f.write(data)
+
---
+++ test_server.py
@@ -0,0 +1,98 @@
+import socket
+import sys
+import threading
+import signal
+from PIL import Image
+from io import BytesIO
+import base64
+import random
+import json
+from action_helper import decode_action, encode_action
+
+
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address given on the command line
+server_address = ('', 9999)
+sock.bind(server_address)
+sock.listen(1)
+
+client_threads = []
+stop_requested = False
+
+
+def signal_handler(signal_, frame_):
+    print 'You pressed Ctrl+C !'
+    global stop_requested
+    stop_requested = True
+    return
+
+
+def handler(connection, client_address):
+    try:
+        print 'client connected:', client_address
+        while not stop_requested:
+            data = recv_msg(connection)
+            if data:
+                print 'received:', len(data)
+                data = json.loads(data)
+                image = Image.open(BytesIO(base64.b64decode(data['img']))).convert('RGB')
+                imgname = 'car_%d.png' % random.randint(0, 10000)
+                image.save(imgname)
+                # print 'received "%s"' % data
+                action = json.dumps(decode_action(2)).zfill(100)
+                connection.sendall(action)
+            else:
+                break
+    finally:
+        connection.close()
+    return
+
+
+def main():
+    while True:
+        print 'waiting for a connection'
+        connection, client_address = sock.accept()
+        client = threading.Thread(target=handler, args=(connection, client_address))
+        client_threads.append(client)
+        if len(client_threads) == 1:
+            break
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    for thread in client_threads:
+        thread.start()
+
+    print 'Press Ctrl+C to stop'
+    signal.pause()
+
+    print 'Now saving data....'
+    for t in client_threads:
+        t.join()
+    return
+
+
+def recv_msg(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = recvall(sock, 20)
+    if not raw_msglen:
+        return None
+    msglen = int(raw_msglen)
+    return recvall(sock, msglen)
+
+
+def recvall(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+    data = ''
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+
+if __name__ == '__main__':
+    main()
